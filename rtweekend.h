425d1ae73d706f4f7086798800c011daabbe3440
hollyclue <77239424+hollyclue@users.noreply.github.com>
2025-03-05 17:58:17 -0500

    Add random number generator

--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,8 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <chrono>
+#include <random>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -14,6 +16,22 @@ constexpr double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(const double degrees) {
     return degrees * pi / 180;
+}
+
+inline std::mt19937 generate() {
+    std::random_device rd{};
+
+    std::seed_seq ss{
+        static_cast<std::seed_seq::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()), rd(), rd(), rd(), rd(), rd(), rd(), rd()
+    };
+
+    return std::mt19937{ss};
+}
+
+inline std::mt19937 mt{ generate() };
+
+inline double random_double() {
+    return std::uniform_real_distribution{0.0, 1.0}(mt);
 }
 
 #include "vec3.h"
